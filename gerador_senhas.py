b96f77ccc2f0955858a8bb44f24719206c3af44c
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-09-09 17:25:10 -0300

    Update gerador_senhas.py

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,11 +1,12 @@
 # Gerador de senhas do iPhone
 
-import random, string
-
-for i in range(3):
-  for _ in range(6):
+import random, string                   # Importando bibliotecas random utilizada para gerar números aleatórios
+                                        # Importando bibliotecas string
+for i in range(3):                      # Para cada elemento i num range de 3 elementos
+  for _ in range(6):                    # Para cada elemento num range de 6 elementos
     print(f'{random.choice(string.digits + string.ascii_letters)}', end='')
-  if i != 2:
-    print('-', end='')
-  else:
-    print('')
+    # imprime um caracter aleatório entre letras e numeros
+  if i != 2:                            # se o elemento i for diferente de 2
+    print('-', end='')                  # imprimir '-' após os seis caracteres aleatórios
+  else:                                 # senão se for igual a 2
+    print('')                           # não imprimir nada (para que não tenha um traço no final da senha)
